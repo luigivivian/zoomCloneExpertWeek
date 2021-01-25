28058e85df101f1e6a2327b8f678a4459977dcb4
luigivivian <luigivivian@hotmail.com>
2021-01-25 20:41:54 -0300

    Aula 1 completa expert week js

--- public/pages/room/src/business.js
+++ public/pages/room/src/business.js
@@ -1,9 +1,10 @@
 class Business{
-    constructor({room, media, view}) {
+    constructor({room, media, view, socketBuilder}) {
         this.room = room;
         this.media = media;
         this.view = view;
-
+        this.socketBuilder = socketBuilder.setOnUserConnected(this.onUserConnected()).build();
+        this.socketBuilder.emit('join-room', this.room, 'teste01');
         this.currentStream = {
 
         };
@@ -22,5 +23,11 @@ class Business{
     addVideoStream(userId, stream= this.currentStream){
         const isCurrentId = false;
         this.view.renderVideo({userId, stream, isCurrentId});
+    }
+
+    onUserConnected = function() {
+        return userId => {
+            console.log('user connected!', userId);
+        }
     }
 }
